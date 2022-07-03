65a37779315edd5b3b400d69b85e5eee158c6f7a
NyergesC <cintia.nyerges@gmail.com>
2022-07-03 17:37:14 +0200

    typical npm package installed

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import Video from '../../videos/video.mp4';
+import Typical from 'react-typical'
 import {Button} from '../ButtonElements';
 import { LandingContainer, LandingBg, VideoBg, LandingContent, LandingH1, LandingP, LandingBtnWrapper, ArrowForward, ArrowRight} from './LandingElements'
 
@@ -19,14 +20,13 @@ const LandingPage = () => {
             <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
         </LandingBg>
         <LandingContent>
-            <LandingH1>Cintia Nyerges</LandingH1>
-            <LandingP>Hi, I am a Web Designer and Frontend Developer with a passion for learning and creating</LandingP>
+            <LandingH1>Welcome to my Portfolio</LandingH1>
+            <LandingP>Hi, my name is Cintia Nyerges</LandingP> 
+            <LandingP>I'm <Typical loop={Infinity} steps={["Frontend Web Developer", 1000, "Webdesigner", 1000, "Enthusiastic Dev", 1000, "React Dev", 1000]}/></LandingP> 
             <LandingBtnWrapper>
                 <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary="true" dark='true'>My Projects {hover ? <ArrowForward /> : <ArrowRight />}</Button>
             </LandingBtnWrapper>
         </LandingContent>
-
-
      </LandingContainer>
 
     </>
